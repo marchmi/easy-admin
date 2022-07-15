3a75d3250851498da18498d229a28b2ff74b9707
zhoushiming <marchmi@163.com>
2022-07-15 16:59:33 +0800

    添加@types/node并设置公共路径

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default {
     base: './',
     plugins: [vue()],
     optimizeDeps: {
-        include: ['schart.js']
+      include: ['schart.js']
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     }
 }
